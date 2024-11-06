7e612266c7f5ca2fedc7e94358d5c0ca97d62e88
victorhugo.lemobs <victorhugo.lemobs@gmail.com>
2024-11-06 00:23:04 -0300

    Correções

--- src/test/java/Testes/Testes_Tarefas.java
+++ src/test/java/Testes/Testes_Tarefas.java
@@ -10,6 +10,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.Assert.assertEquals;
 
@@ -19,7 +20,7 @@ public class Testes_Tarefas {
     @Before
     public void setUp() {
         // Caminho para o WebDriver do Chrome (ou outro navegador)
-
+        WebDriver driver = new ChromeDriver();
         driver = Driver.configurarChromeDriver();
 
 //        Driver wdriver = new Driver();
