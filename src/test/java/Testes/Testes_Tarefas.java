d2fbb6b40f4083610c215200e7a57b110048b55c
victorhugo.lemobs <victorhugo.lemobs@gmail.com>
2024-11-06 13:17:40 -0300

    Correções

--- src/test/java/Testes/Testes_Tarefas.java
+++ src/test/java/Testes/Testes_Tarefas.java
@@ -23,8 +23,7 @@ public class Testes_Tarefas {
     public void setUp() {
         // Caminho para o WebDriver do Chrome (ou outro navegador)
         ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--lang=pt-br", "--headless","--no-sandbox");
-        options.addArguments("--lang=pt","--no-sandbox");
+        options.addArguments("--lang=pt-br", "--headless","--no-sandbox");
 
         driver = new ChromeDriver(options);
         driver.get("http://mantis-prova.base2.com.br");
